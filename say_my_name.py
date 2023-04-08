671aa744b7a12579cc2a410ccee77427eada0569
vasu-devops <yev.kesavarao@gmail.com>
2023-04-08 09:21:58 -0400

    adding dummpy python code and test coverage

---
+++ say_my_name.py
@@ -0,0 +1,9 @@
+def say_my_name(name=None):
+    if name != "":
+        print(name, " ??? You are right !!!")
+    else:
+        print("Heisenberg ??? You are right !!!")
+
+
+if __name__ == "__main__":
+    say_my_name(input("Now.. Say My Name? "))
---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+from io import StringIO
+from unittest.mock import patch
+from say_my_name import say_my_name
+
+
+class TestHandlerCase(unittest.TestCase):
+
+    def test(self):
+        name = 'testmycode'
+        expected_output = '{} ??? You are right !!!\n'.format(name)
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            say_my_name(name)
+            self.assertEqual(fake_out.getvalue(), expected_output)
+
+if __name__ == "__main__":
+    unittest.main()          
